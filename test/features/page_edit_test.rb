11d8b92a5eb169851bab568c2fb860c216eb8b61
Jeff Casimir <jeff@casimircreative.com>
2014-08-04 08:24:27 -0600

    Pseudocode for the Edit test

---
+++ test/features/page_edit_test.rb
@@ -0,0 +1,14 @@
+require './test/test_helper'
+
+class PageEditTest < FeatureTest
+  def test_it_edits_a_page
+    skip
+    
+    # Create a page
+    # Visit it and make sure the content is there
+    # Click an EDIT link on that page
+    # Change the content via the form
+    # Submit the form
+    # Verify that you're back to the article and "see" the updated content
+  end
+end
