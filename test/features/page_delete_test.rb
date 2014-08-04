d69f50392151221c2aaac669abb043325691d755
Jeff Casimir <jeff@casimircreative.com>
2014-08-04 08:22:17 -0600

    Add the pseudocode for the deletion test

---
+++ test/features/page_delete_test.rb
@@ -0,0 +1,12 @@
+require './test/test_helper'
+
+class PageDeleteTest < FeatureTest
+  def test_it_deletes_a_page
+    skip
+    
+    # Create two pages in the database
+    # Make a delete request to delete one of them
+    # Make sure the deleted one is gone (404)
+    # Make sure the other one is still there
+  end
+end
