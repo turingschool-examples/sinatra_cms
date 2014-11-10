0ba238da209397625959e86f52d5ce6d8a8ca867
Jeff Casimir <jeff@casimircreative.com>
2014-11-10 09:28:17 -0700

    Undoing accidental commit of auth experiments

--- lib/app.rb
+++ lib/app.rb
@@ -15,23 +15,7 @@ class CMS < Sinatra::Base
   end
 
   get '/pages/:slug' do |slug|
-    if slug == 'sample_2'
-      protected!
-    end
-
     page = Page.find_by_slug(slug)
     erb :page, :locals => {:page => page}
-  end
-
-  def protected!
-    return if authorized?
-    headers['WWW-Authenticate'] = 'Basic realm="Jimmy\'s BBQ"'
-    halt 401, "Not authorized\n"
-  end
-
-  def authorized?
-    @auth ||= Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? && @auth.basic? &&
-    @auth.credentials && @auth.credentials == ['admin', 'admin']
   end
 end
